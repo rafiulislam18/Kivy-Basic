5df421e1324b22d1f8838efe9c66a1a4045641a3
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-12 12:25:07 +0600

    Kivy - Line Drawing (Line, Canvas)

---
+++ 6. Kivy - Line Drawing (Line, Canvas).py
@@ -0,0 +1,20 @@
+# Kivy - Line Drawing (Line, Canvas)
+
+from kivy.app import App
+from kivy.graphics import Line
+from kivy.uix.widget import Widget
+
+class MyWidget(Widget):
+    def on_touch_down(self, touch):
+        touch.ud['line'] = Line(points=(touch.x, touch.y))
+        self.canvas.add(touch.ud['line'])
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
+class MyApp(App):
+    def build(self):
+        return MyWidget()
+    
+if __name__ == '__main__':
+    MyApp().run()
