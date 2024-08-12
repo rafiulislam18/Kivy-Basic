dadebeec4607ae102ac3b8e463d68b58e944e4a1
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-12 09:59:56 +0600

    Kivy Button

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 class MyApp(App):
     def build(self):
-        layout = BoxLayout()
+        layout = BoxLayout(orientation='vertical')
         label1 = Label(text='Hello World')
-        label2 = Label(text='Button 2')
+        label2 = Label(text='Label 2')
+        button = Button(text='Press Me')
         layout.add_widget(label1)
         layout.add_widget(label2)
+        layout.add_widget(button)
 
         return layout
     
