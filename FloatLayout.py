fac54ba90588617bda81a92e0eb80f170764c7c4
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-12 10:27:05 +0600

    Absolute Position in Kivy

--- FloatLayout.py
+++ FloatLayout.py
@@ -3,16 +3,20 @@
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 
 class MyApp(App):
     def build(self):
         layout = FloatLayout()
+        
+        # Relative positioning
         label1 = Label(text='hello', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.2, 'center_y': 0.5})
-        label2 = Label(text='there', size_hint=(0.5, 0.7), pos_hint={'center_x': 0.5, 'center_y': 0.1})
+        # Absolute positioning
+        button = Button(text='click', size=(50,50), size_hint=(None,None), pos=(100,200))
 
         layout.add_widget(label1)
-        layout.add_widget(label2)
+        layout.add_widget(button)
 
         return layout
     
